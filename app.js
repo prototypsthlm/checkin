f746f20877bd25fb4e705d2620161bea584e473e
jacob broms <jacob.broms@sanocore.se>
2013-09-16 14:09:58 +0200

    gpio

--- app.js
+++ app.js
@@ -33,3 +33,46 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+
+var gpio = require("gpio");
+var gpio22, gpio4, intervalTimer;
+
+// Flashing lights if LED connected to GPIO22
+gpio22 = gpio.export(22, {
+  ready: function() {
+    intervalTimer = setInterval(function() {
+      gpio22.set();
+      console.log("gpio22: ", gpio22.value);
+      setTimeout(function() { gpio22.reset(); }, 500);
+    }, 1000);
+  }
+});
+
+// Lets assume a different LED is hooked up to pin 4, the following code 
+// will make that LED blink inversely with LED from pin 22 
+gpio4 = gpio.export(4, {
+  ready: function() {
+    // bind to gpio22's change event
+    gpio22.on("change", function(val) {
+      gpio4.set(1 - val); // set gpio4 to the opposite value
+      console.log("gpio4: ", gpio4.value);
+    });
+  }
+});
+
+// reset the headers and unexport after 10 seconds
+setTimeout(function() {
+  clearInterval(intervalTimer);          // stops the voltage cycling
+  gpio22.removeAllListeners('change');   // unbinds change event
+  gpio22.reset();                        // sets header to low
+  gpio22.unexport();                     // unexport the header
+
+  gpio4.reset();
+  gpio4.unexport(function() {
+    // unexport takes a callback which gets fired as soon as unexporting is done
+    //process.exit(); // exits your node program
+  });
+}, 10000);
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,35 @@
  * GET home page.
  */
 
+//var gpio = require("gpio");
+
+// Calling export with a pin number will export that header and return a gpio header instance
+
+/*var gpio4 = gpio.export(4, {
+	// When you export a pin, the default direction is out. This allows you to set
+	// the pin value to either LOW or HIGH (3.3V) from your program.
+	direction: 'out',
+
+	// set the time interval (ms) between each read when watching for value changes
+	// note: this is default to 100, setting value too low will cause high CPU usage
+	interval: 200,
+
+	// Due to the asynchronous nature of exporting a header, you may not be able to
+	// read or write to the header right away. Place your logic in this ready
+	// function to guarantee everything will get fired properly
+	ready: function() {
+
+	}
+});*/
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+	/*gpio4.set(function() {
+		console.log(gpio4.value); // should log 1
+		res.render('index', { title: gpio4.value });
+	});*/
+
+	res.render('index', { title: gpio4.value });
+
+
 };
